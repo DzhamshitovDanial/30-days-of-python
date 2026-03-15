767d788bac1a54690519694dbe2264c848aa360c
Dzhamshitov Danial <finmotive7@gmail.com>
2026-03-15 12:47:47 +0500

    30days of python

---
+++ day5/day 5.py
@@ -0,0 +1,19 @@
+#unpacking list
+list_1=['green','red','yellow']
+a,b,c=list_1
+print(a)
+print(b)
+print(c)
+#reversing list
+list_1.reverse()
+print(list_1)
+#sorting list
+#asc
+list_1.sort()
+print(list_1)
+#desc
+list_1.sort(reverse=True)
+print(list_1)
+#plus the list
+list_2=['purple','black']
+print(list_1+list_2)
