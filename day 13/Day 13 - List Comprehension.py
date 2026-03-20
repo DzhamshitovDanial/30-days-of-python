b34a6a39a231558b870ecdf5050c505f3e2830d7
Dzhamshitov Danial <finmotive7@gmail.com>
2026-03-20 14:34:39 +0500

    day 13

---
+++ day 13/Day 13 - List Comprehension.py
@@ -0,0 +1,10 @@
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+lst=[i for i in numbers if i<=0]
+print (lst)
+#
+list_of_lists =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+lst=[j for i in list_of_lists for j in i]
+print(lst)
+#
+lst=[(i,i**0  ,i**1,i**2,i**3,i**4,i**5) for i in range(11)]
+print(*lst,sep='\n')
